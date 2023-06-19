108a456a33fb748d01d33c3e8e9fe2f13010cd9a
marta-valle <m.limavalle.ti@gmail.com>
2023-06-18 22:11:34 -0300

    Implementação de métodos no Controller Aluno e criação de IAlunoRespository

--- src/main/java/br/com/cotiinformatica/controlleres/AlunosControlller.java
+++ src/main/java/br/com/cotiinformatica/controlleres/AlunosControlller.java
@@ -1,5 +1,10 @@
 package br.com.cotiinformatica.controlleres;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,38 +16,135 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cotiinformatica.dto.AlunosPostDTO;
 import br.com.cotiinformatica.dto.AlunosPutDTO;
+import br.com.cotiinformatica.entities.Aluno;
+import br.com.cotiinformatica.repositories.IAlunoRepository;
 
+//2º fase: Implementações de métodos
 @RestController
 @RequestMapping(value = "api/alunos")
 public class AlunosControlller {
 
+	@Autowired
+	private IAlunoRepository iAlunoRepository;
+
 	@PostMapping
-	public void Post(@RequestBody AlunosPostDTO dto) {
-		// TODO: POST
-		/*Retirar o Void ; incluir "ResponseEntity e o tipo de retorno<String>
-		*Mensagens: 200: Sucesso
-		*Mensagem 400: Fez a busca, mas não foi encontrado.
-		*Mensagem 500: Falha no desenvolvimento
-		*/
+	public ResponseEntity<String> Post(@RequestBody AlunosPostDTO dto) {
+		try {
+			// INICIANDO ALUNO
+			Aluno aluno = new Aluno();
+
+			// RECEBENDO INFORMAÇÕES DO DTO - COMO É CADASTRO NOVO NÃO PRECISA DE ID
+			aluno.setNomeAluno(dto.getNomeAluno());
+			aluno.setCpf(dto.getCpf());
+			aluno.setMatricula(dto.getMatricula());
+
+			// GRAVANDO O QUE RECEBEMOS DO DTO SALVANDO NA TABELA ALUNO
+			iAlunoRepository.save(aluno);
+
+			// RETORNANDO MENSAGEM
+			return ResponseEntity.status(201).body("Cadastro de Aluno realizado com sucesso!");
+		} catch (Exception e) {
+			// CASO DE FALHA EM DESENVOLVIMENTO.
+			return ResponseEntity.status(500).body("Erro:" + e.getMessage());
+		}
+
 	}
 
 	@PutMapping
-	public void Put(@RequestBody AlunosPutDTO dto) {
-		// TODO: PUT
+	public ResponseEntity<String> Put(@RequestBody AlunosPutDTO dto) {
+		try {
+			// INICIANDO TABELA ALUNO
+			Aluno aluno = new Aluno();
+
+			// PRECISAMOS CONSULTAR SE O ALUNO QUE QUEREMOS ALTERAR, JÁ TEMOS NO BD
+			// Método Optional do SpringData, executa o método do ialunorepository, buscando
+			// o valor recebido pelo dto (idAluno)
+			Optional<Aluno> optional = iAlunoRepository.findById(dto.getIdAluno());
+
+			// OPTIONAL DEVERA SER "QUESTIONADO" SE O ALUNO FOI ENCONTRADO
+			// OPTIONAL O ALUNO COM IDxxx FOI ENCONTRADO? SE SIM, EXECUTE:
+			if (optional.isPresent()) {
+				//LEMBRAR DE INCLUIR O ID QUE SERÁ ALTERADO
+				aluno.setIdAluno(dto.getIdAluno());
+				aluno.setNomeAluno(dto.getNomeAluno());
+				aluno.setCpf(dto.getCpf());
+				aluno.setMatricula(dto.getMatricula());
+				
+				//SALVANDO AS ALTERAÇÕES
+				iAlunoRepository.save(aluno);
+
+				return ResponseEntity.status(201).body("Alteração realizada sucesso");
+			}
+			// SE NÃO EXECUTE:
+			else {
+				return ResponseEntity.status(404).body("Aluno não encontrado, para realizar alteração");
+			}
+
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body("Erro:" + e.getMessage());
+		}
 	}
 
 	@DeleteMapping("{idAluno}")
-	public void Put(@PathVariable("idAluno") Integer idAluno) {
-		// TODO: DELETE
+	public ResponseEntity<String> delete(@PathVariable("idAluno") Integer idAluno) {
+		// INICIANDO TABELA ALUNO
+		
+		try {
+			// PRECISAMOS CONSULTAR SE O ALUNO QUE QUEREMOS DELERAR, REALMENTE EXISTE
+			Optional<Aluno> optional = iAlunoRepository.findById(idAluno);
+
+			if (optional.isPresent()) {
+				Aluno aluno = optional.get();
+				iAlunoRepository.delete(aluno);
+
+				return ResponseEntity.status(201).body("Exclusão do aluno realizada sucesso");
+			} else {
+
+				return ResponseEntity.status(404).body("Aluno não encontrado, por favor verifique o IdAluno");
+			}
+
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body("Erro:" + e.getMessage());
+		}
+
 	}
 
 	@GetMapping
-	public void GetAll() {
-		// TODO: GETALL
+	public ResponseEntity<List<Aluno>> getAll() {
+		// O RETORNO SERÁ EM LISTA
+		try {
+
+			// Criamos uma LIST para retornar os dados capturados pelo iAlunosRepository
+			// (Método já criado pelo SpringData)
+
+			List<Aluno> aluno = iAlunoRepository.findAll();
+
+			return ResponseEntity.status(200).body(aluno);
+
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body(null);
+			// COMO O TIPO DE RETORNO É LISTA, SE NÃO HOUVER CADASTRO O RETORNO É NULL
+		}
+
 	}
 
 	@GetMapping("{idAluno}")
-	public void GetFindById(@PathVariable("idAluno") Integer idAluno) {
-		// TODO: GETFINDBYID
+	public ResponseEntity<Aluno> GetFindById(@PathVariable("idAluno") Integer idAluno) {
+		try {
+
+			Optional<Aluno> optional = iAlunoRepository.findById(idAluno);
+			if (optional.isPresent()) {
+				Aluno aluno = optional.get();
+
+				return ResponseEntity.status(200).body(aluno);
+
+			} else {
+				return ResponseEntity.status(404).body(null);
+			}
+
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body(null);
+		}
+
 	}
 }
